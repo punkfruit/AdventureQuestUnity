213a609fae24a973e84e79480dcfb5087dfbf0d8
punkfruit <thecybergrape@gmail.com>
2023-07-16 20:11:11 -0600

    inventoryStart

---
+++ AdventureQuestRemake/Assets/scripts/InventoryItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum itemType { greenPotion, redPotion }
+public class InventoryItem 
+{
+    public string name = "Item";
+    public int icon;
+}
